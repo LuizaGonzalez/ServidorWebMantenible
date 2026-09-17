fb44fb683efcbac930559aa491c2fa6a09c91655
Luiza Gonzalez <lulogove42@gmail.com>
2026-09-17 11:29:07 -0500

    Refactor: route in web page

--- ServidorWebMantenible/src/main/resources/webroot/app.js
+++ ServidorWebMantenible/src/main/resources/webroot/app.js
@@ -7,3 +7,33 @@ function greet() {
                 document.getElementById("result").innerHTML = message;
     });
 }
+function getPi() {
+    fetch("/pi")
+        .then(response => response.text())
+        .then(message => {
+            document.getElementById("resultPi").innerHTML = "π = " + message;
+        });
+}
+
+function getE() {
+    fetch("/e")
+        .then(response => response.text())
+        .then(message => {
+            document.getElementById("resultE").innerHTML = "e = " + message;
+        });
+}
+
+function getSquare() {
+    const value = document.getElementById("value").value;
+
+    if (value === "" || value === null) {
+        document.getElementById("resultSquare").innerHTML = "Introduzca un número";
+        return;
+    }
+
+    fetch("/square?value=" + encodeURIComponent(value))
+        .then(response => response.text())
+        .then(message => {
+            document.getElementById("resultSquare").innerHTML = message;
+        });
+}
